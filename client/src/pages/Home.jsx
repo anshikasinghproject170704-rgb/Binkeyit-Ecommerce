cd912627a2b656e8e7ee70e00b162a1510981c39
anshikasinghproject170704-rgb <anshikasingh.project170704@gmail.com>
2026-04-26 18:09:20 +0530

    Update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -13,6 +13,7 @@ import banner from '../assets/banner.jpg'
 import bannerMobile from '../assets/banner-mobile.jpg'
 import CategoryWiseProductDisplay from '../Components/CategoryWiseProductDisplay'
 import { valideURLConvert } from '../utils/valideURLConvert'
+import SmartKits from '../Components/SmartKits'
 const Home = () => {
   const loadingCategory = useSelector(state => state.product.loadingCategory)
   const categoryData = useSelector(state => state.product.allCategory)
@@ -160,6 +161,7 @@ const Home = () => {
                           </div>
                       ))
                     ) : (
+               <SmartKits />
                       categoryData.map((cat) => (
                           <div 
                             key={cat._id} 
